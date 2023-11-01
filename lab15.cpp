318c5241ae43ae2683c5b2443fc4277625768e5d
zigzag01 <91686105+zigzag01@users.noreply.github.com>
2023-11-01 13:24:45 +0300

    Update lab15.cpp

--- lab15.cpp
+++ lab15.cpp
@@ -2,43 +2,42 @@
 #include <iomanip>
 using namespace std;
 
-void MainMatrix(int** a, int n, int m) //вывод эл-тов матрицы
-{
-	int i, j;
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < m; j++) {
-			cout << setw(5) << a[i][j];
-		}
-		cout << endl;
-	}
+void MatrixSize(int &m, int &n) {
+    do {
+        cout << "Кол-во строк массива: ";
+        cin >> n;
+    }
+    while (n > 150);
+    do {
+        cout << "Кол-во столбцов массива: ";
+        cin >> m;
+    }
+    while (m > 150);
 }
 
+
+
 int main() {
-	setlocale(LC_ALL, "rus");
-
-	int n, m, i, j;
-	int** a;
-	int* tmp = new int [n];
-	a = new int* [n];
-	tmp = new int[n];
-
-	cout << "Введите n: ";
-	cin >> n;
-	cout << "Введите m: ";
-	cin >> m;
-
-	for (i = 0; i < n; i++) {
-		a[i] = new int[m];
-	}	
-	cout << "Items=";
-	for (i = 0; i < n; i++) {
-		for (j = 0; j < m; j++) {
-			cin >> a[i][j];
-		}
-	}
-		
-	MainMatrix(a, n, m);
-
-
+    setlocale (LC_ALL, "rus");
+    
+    int n, m, i, j;
+    int A[150][150]; //матрица размера n × m
+    MatrixSize(n, m);
+    
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            A[i][j] = -100 + rand() % 201; //числа от -100 до 100 
+        }
+    }
+    
+    //вывод матрицы
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            cout << A[i][j] << " "; 
+        }
+        cout << endl;
+    }
+    
+    
+    
 }
